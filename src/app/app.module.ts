68b08c7c836b0047ddab709359acf5cb54f1b4c8
David Ayeke <ayekedavid@yahoo.com>
2018-01-23 18:28:53 -0600

    [General] Added Timeline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,6 +65,7 @@ import { AnnotationsService } from './services/annotations/annotations.service';
 import { VisualizationService } from './services/visualization/visualization.service';
 import { MeasurementsService } from './services/measurements/measurements.service';
 import { ShotplansService } from './services/shotplans/shotplans.service';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
   return localStorageSync({
@@ -106,6 +107,7 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
     DatasetAnnotationDetailsComponent,
     ShotplanningToolComponent,
     SigninSignupLayoutComponent,
+    TimelineComponent,
   ],
   entryComponents: [
     NewAnnotationFormComponent
