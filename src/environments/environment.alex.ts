c660f8a546085d40586f6db0b5906f2952d0e931
Alex Martinosky <cry0fyr3@gmail.com>
2018-01-24 04:39:04 +0000

    Added configuration data for Alex

---
+++ src/environments/environment.alex.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  http: 'http',
+  be_host: 'devx.strayos.com',
+  be_port: '3002',
+  api_prefix: 'api',
+  mapbox_key: 'pk.eyJ1IjoiZG1pdHJpeXN0cmF5b3MiLCJhIjoiY2piN2htNHdvMHdpeTMzcXpvYXJ3cm15eCJ9.99yHJs52fCMkRpHxGcNVIw',
+};
